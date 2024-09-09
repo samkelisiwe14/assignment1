3b70f027b1d6a9a55761857352bf73a57cdfb994
RC_Student_lab <RC_Student_lab@10.0.1.19>
2024-09-09 07:28:58 +0000

    first commit

---
+++ src/main/java/com/mycompany/firstprojectt0github/FirstProjectT0GitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstprojectt0github;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class FirstProjectT0GitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
