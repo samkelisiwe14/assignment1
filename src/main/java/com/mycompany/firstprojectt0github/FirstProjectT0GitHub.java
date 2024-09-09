0215adba109c576e160669687c2043c2f0f3502d
RC_Student_lab <RC_Student_lab@10.0.1.19>
2024-09-09 08:24:29 +0000

    created a program to add two numbers

--- src/main/java/com/mycompany/firstprojectt0github/FirstProjectT0GitHub.java
+++ src/main/java/com/mycompany/firstprojectt0github/FirstProjectT0GitHub.java
@@ -11,6 +11,9 @@ package com.mycompany.firstprojectt0github;
 public class FirstProjectT0GitHub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a =5;
+        int b= 10;
+        int c = a+b;
+        System.out.println("sum:" + c);
     }
 }
